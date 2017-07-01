a9fb2ea5aac1c59e4a5dc124894b9ef217d8c8d7
Arshia shakeri <arshshia@gmail.com>
2017-07-01 16:14:18 +0430

    Update addcounter.py

--- plugins/addcounter.py
+++ plugins/addcounter.py
@@ -4,6 +4,12 @@ def addcounter(message):
   zigzag.nextstep(m, adcstep2)
 
 
-class plecho:
+class pladdcounter:
   patterns = ["^[!/]addcounter(.*)$"]
   # At the 'inlines' variable, you can use DEFAULTQUERY to get when there is no queries entered
+
+def adcstep2(message):
+  messgid = message.message_id
+  messgchatid = message.chat.id
+  fw = bot.forward_message("@ZigZagPrivZZZZZ", from_chat_id=messgchatid, message_id=messgid)
+  bot.forward_message(message.chat.id, from_chat_id=fw.chat.id, message_id=fw.message_id)
