b680cfd7ba48a7b631944c7e48e694c073b9ebd6
Arshia shakeri <arshshia@gmail.com>
2017-07-01 16:15:04 +0430

    Update config.default.py

--- data/config.default.py
+++ data/config.default.py
@@ -1,9 +1,10 @@
 config = {
-"token" : "blahblahblah",
+"token" : "182760465:AAHsDEDRDZMKc_Rm3uji3TvYDrEOgFi-l1M",
 "sudo" : ["98120772"],
 "loggp" : 1000000000000,
 }
 
 enabled_plugins = [
 "echo",
+"addcounter",
 ]
