e5cbd308ee0f9b36dff541f440202acb95859ce2
Arshia shakeri <arshshia@gmail.com>
2017-07-01 16:29:54 +0430

    Update banhammer.py

--- plugins/banhammer.py
+++ plugins/banhammer.py
@@ -1,1 +1,14 @@
+def banhammer(message):
+  if message.from_user.id not in sudo:
+    return
+  if len(message.text.split()) < 2:
+    bot.reply_to(message, "Who should I ban?")
+    return
+  userid = message.text.split()[1].replace(" ", "")
+  if message.text.split()[0] == "/ban":
+    zigzag.ban(userid)
+  elif message.text.split()[0] == "/unban":
+    zigzag.unban(userid)
 
+class plbanhammer:
+  patterns = ["^[!/]unban (.*)$", "^[!/]ban (.*)$"]
