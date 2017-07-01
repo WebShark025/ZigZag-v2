b732f0eb5ad4fdbeef69d1f0a1da81405d69e4e0
Arshia shakeri <arshshia@gmail.com>
2017-07-01 16:12:40 +0430

    Update addcounter.py

--- plugins/addcounter.py
+++ plugins/addcounter.py
@@ -1,17 +1,9 @@
 def addcounter(message):
   userid = message.from_user.id
-  userlang = redisserver.get("settings:user:language:" + str(message.from_user.id))
-  banlist = redisserver.sismember('zigzag_banlist', '{}'.format(userid))
-  if banlist:
-    return
-  if len(message.text.split()) < 2:
-    bot.reply_to(message, language[userlang]["ADDCOUNTER_NEA_MSG"].encode("utf-8"), parse_mode="Markdown")
-    return
-  if (message.text.split()[1] == "add"):
-    if userid in addcntr:
-      return
-    bot.send_message(message.chat.id, language[userlang]["ADDCOUNTER_SENDNOW_MSG"])
-    addcntr.append(userid)
-  else:
-    bot.reply_to(message, language[userland]["ADDCOUNTER_NEA_MSG"].encode("utf-8"), parse_mode="Markdown")
-    return
+  m = bot.send_message(message.chat.id, "Please send your message so it will get add-countererd")
+  zigzag.nextstep(m, adcstep2)
+
+
+class plecho:
+  patterns = ["^[!/]addcounter(.*)$"]
+  # At the 'inlines' variable, you can use DEFAULTQUERY to get when there is no queries entered
