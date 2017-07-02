4b9fc1ddca4b0463c565aa5d05d5e68e424c6a00
Arshia shakeri <arshshia@gmail.com>
2017-07-02 11:12:42 +0430

    Update lmgtfy.py

--- plugins/lmgtfy.py
+++ plugins/lmgtfy.py
@@ -1,1 +1,9 @@
-
+def lmgtfغ(message):
+  userlang = redisserver.get("settings:user:language:" + str(message.from_user.id))
+  userid = message.from_user.id
+  if len(message.text.split()) < 2:
+    bot.reply_to(message, language[userlang]["LMGTFY_NEA_MSG"], parse_mode="Markdown")
+    return
+  textl = message.text.replace("/lmgtfy ","", 1).replace("/Lmgtfy ", "", 1).replace("+","%2B")
+  rez = urllib.urlopen("http://r1z.ir/api.php?long=http://lmgtfy.com/?q={}".format(textl)).read()
+  bot.send_message(message.chat.id, "Direct link: `{}`\n\nOr click on [this :D]({})".format(rez,rez), parse_mode="Markdown", disable_web_page_preview=True)
