240d6e6fcd056121fec0d22fe1397a0abb654974
Arshia shakeri <arshshia@gmail.com>
2017-07-02 11:00:36 +0430

    Update qrcode.py

--- plugins/qrcode.py
+++ plugins/qrcode.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 
 def qrcode(message):
-  userlang = redisserver.get("settings:user:language:" + str(message.from_user.id))
   userid = message.from_user.id
-  banlist = redisserver.sismember('zigzag_banlist', '{}'.format(userid))
-  if banlist:
-    return
-  if len(message.text.replace("◻️ QR Code", "", 1).split()) < 2:
-    bot.reply_to(message, language[userlang]["QRCODE_NEA_MSG"], parse_mode="Markdown")
-    return
-  argus = message.text.replace("/qrcode ","").replace(" ", "%20")
-  bot.reply_to(message, "Processing..")
-  urllib.urlretrieve("http://api.qrserver.com/v1/create-qr-code/?data={}&size=600x600".format(argus), 'qrcode.png')
-#  print("http://apimeme.com/meme?meme={}&top={}&bottom={}".format(args[0], args[1], args[2]))
-  bot.send_photo(message.chat.id, open('qrcode.png'), caption=" QR Code by @TheZigZagBot")
+  m = bot.send_message(message.chat.id, "Please send the link/address/text/etc. you want to be converted to QR-Code")
+  zigzag.nextstep(m, "qrnext")
+
+def qrnext(message):
+  args = message.text.replace("/qrcode ","").replace(" ", "%20")
+  bot.reply_to(message, "Processing... Please wait !")
+  urllib.urlretrieve("http://api.qrserver.com/v1/create-qr-code/?data={}&size=600x600".format(args), 'data/qr{}.png'.format(userid))
+  bot.send_photo(message.chat.id, open('data/qr{}.png'.format(userid)), caption="QR Code by @TheZigZagBot")
+
+class plqrcode:
+  patterns = ["^[/!]qrcode(.*)$"]
