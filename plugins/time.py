82c488ad550dc5b7b11f270973220c0df5975676
Arshia shakeri <arshshia@gmail.com>
2017-07-03 16:01:01 +0430

    Update time.py

--- plugins/time.py
+++ plugins/time.py
@@ -1,14 +1,19 @@
 
-def time_message(message):
-  userlang = redisserver.get("settings:user:language:" + str(message.from_user.id))
+def time(message):
   userid = message.from_user.id
-  banlist = redisserver.sismember('zigzag_banlist', '{}'.format(userid))
-  if banlist:
-    return
   if len(message.text.split()) < 2:
-    bot.reply_to(message, language[userlang]["TIME_NEA_MSG"], parse_mode="Markdown")
+    m = bot.send_message(message.chat.id, "Please send the city you want to get it's time in your next message.")
+    zigzag.nextstep(m, "timecity")
     return
-  city = message.text.split()[1]
+  
+  bot.send_chat_action(message.chat.id, "typing")
+  time = json.load(urllib.urlopen("https://script.google.com/macros/s/AKfycbyd5AcbAnWi2Yn0xhFRbyzS4qMq1VucMVgVvhul5XqS9HkAyJY/exec?tz={}".format(timezone)))
+  bot.send_message(message.chat.id, "Current time in *" + timezone + "*: \n" + time["fulldate"], parse_mode="Markdown")
+
+def timecity(message):
+  
+
+def gettime(city):
   try:
     tzd = json.load(urllib.urlopen("https://maps.googleapis.com/maps/api/geocode/json?address={}".format(city)))
     if str(tzd["status"]) == "OK":
@@ -23,7 +28,3 @@ def time_message(message):
   except:
     print("[Time] Exception occured")
     return
-#  bot.send_chat_action(message.chat.id, "typing")
-#  tm.sleep(1)
-  time = json.load(urllib.urlopen("https://script.google.com/macros/s/AKfycbyd5AcbAnWi2Yn0xhFRbyzS4qMq1VucMVgVvhul5XqS9HkAyJY/exec?tz={}".format(timezone)))
-  bot.send_message(message.chat.id, "Current time in *" + timezone + "*: \n" + time["fulldate"], parse_mode="Markdown")
